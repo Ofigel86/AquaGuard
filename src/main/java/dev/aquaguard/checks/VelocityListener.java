eff9fda0976de8864df02e582c8f289d45e2e172
Ofigel86 <michaofigel@gmail.com>
2025-08-10 12:27:25 +0300

    VelocityListener.java

--- src/main/java/dev/aquaguard/checks/VelocityListener.java
+++ src/main/java/dev/aquaguard/checks/VelocityListener.java
@@ -5,10 +5,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerVelocityEvent;
 
+/**
+ * Отмечает время последнего velocity, чтобы чеки движения учитывали иммунитет
+ * (например, Speed/Fly не флагали сразу после нокбэка/рывка).
+ */
 public class VelocityListener implements Listener {
     private final DataManager data;
 
-    public VelocityListener(DataManager data) { this.data = data; }
+    public VelocityListener(DataManager data) {
+        this.data = data;
+    }
 
     @EventHandler
     public void onVelocity(PlayerVelocityEvent e) {
