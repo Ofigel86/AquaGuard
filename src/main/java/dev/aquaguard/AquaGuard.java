4039da337d4585dd1299721f0d94541272c1d861
Ofigel86 <michaofigel@gmail.com>
2025-08-10 10:02:40 +0300

    AquaGuard.java

--- src/main/java/dev/aquaguard/AquaGuard.java
+++ src/main/java/dev/aquaguard/AquaGuard.java
@@ -1,10 +1,10 @@
 package dev.aquaguard;
 
-import dev.aquaguard.checks.MovementListener;
-import dev.aquaguard.checks.VelocityListener;
-import dev.aquaguard.checks.WorldListener;
+import dev.aquaguard.bypass.BypassManager;
+import dev.aquaguard.checks.*;
 import dev.aquaguard.core.DataManager;
 import dev.aquaguard.core.ViolationManager;
+import dev.aquaguard.freeze.FreezeManager;
 import dev.aquaguard.gui.GuiManager;
 import dev.aquaguard.owner.OwnerAccess;
 import dev.aquaguard.penalty.PenaltyManager;
@@ -18,6 +18,13 @@ public class AquaGuard extends JavaPlugin {
     private DataManager dataManager;
     private PenaltyManager penaltyManager;
     private OwnerAccess ownerAccess;
+
+    // services
+    private BypassManager bypassManager;
+    private CheckManager checkManager;
+    private FreezeManager freezeManager;
+
+    // gui
     private GuiManager guiManager;
 
     @Override
@@ -25,36 +32,38 @@ public class AquaGuard extends JavaPlugin {
         instance = this;
         saveDefaultConfig();
 
-        // Core
+        // core
         this.violationManager = new ViolationManager(this);
         this.violationManager.load();
-
         this.dataManager = new DataManager();
         this.penaltyManager = new PenaltyManager(this, violationManager);
         this.ownerAccess = new OwnerAccess(this);
 
-        // GUI
-        this.guiManager = new GuiManager(this, violationManager, penaltyManager);
+        // services
+        this.bypassManager = new BypassManager(this);
+        this.checkManager = new CheckManager(this);
+        this.freezeManager = new FreezeManager();
 
-        // Listeners
+        // gui
+        this.guiManager = new GuiManager(this, violationManager, penaltyManager, checkManager, bypassManager, freezeManager);
+
         var pm = getServer().getPluginManager();
         pm.registerEvents(ownerAccess, this);
         pm.registerEvents(dataManager, this);
-        pm.registerEvents(new MovementListener(this, dataManager, violationManager), this);
+        pm.registerEvents(new MovementListener(this, dataManager, violationManager, checkManager, bypassManager, freezeManager), this);
         pm.registerEvents(new VelocityListener(dataManager), this);
-        pm.registerEvents(new WorldListener(this, dataManager, violationManager), this);
+        pm.registerEvents(new WorldListener(this, dataManager, violationManager, checkManager, bypassManager), this);
+        pm.registerEvents(new CombatListener(this, dataManager, violationManager, checkManager, bypassManager), this);
         pm.registerEvents(penaltyManager, this);
         pm.registerEvents(guiManager, this);
 
-        // Decay VL раз в минуту
         long minute = 20L * 60L;
         getServer().getScheduler().runTaskTimer(this,
                 () -> violationManager.decayAll(getConfig().getDouble("decay-per-minute", 0.5)),
                 minute, minute);
 
-        // Команда /ag
         if (getCommand("ag") != null) {
-            getCommand("ag").setExecutor(new AgCommand(this, violationManager, penaltyManager, ownerAccess, guiManager));
+            getCommand("ag").setExecutor(new AgCommand(this, violationManager, penaltyManager, ownerAccess, guiManager, bypassManager, checkManager, freezeManager));
         }
 
         getLogger().info("AquaGuard enabled.");
